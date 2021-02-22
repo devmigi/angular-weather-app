8b3f3d914b16039bfcadf5f24567e9333a800455
Mrigendra <mrigendra.kumar@edureka.co>
2021-02-22 19:48:23 +0530

    completed weather fetch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherAppId: "04882dfb9a280647dee1fd9967d318e8",
+  weatherApiUrl: 'http://api.openweathermap.org/data/2.5/weather'
 };
 
 /*
